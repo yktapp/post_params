31584533fefa295dfdcd39157eab1da8d6c87450
Александр Саввинов <savvinovan@gmail.com>
2020-02-06 16:18:24 +0900

    added get array

--- post_param.go
+++ post_param.go
@@ -38,3 +38,73 @@ func GetBool(r *http.Request, key string) (value bool) {
 func GetString(r *http.Request, key string) string {
 	return r.PostFormValue(key)
 }
+
+func GetArrayInt(r *http.Request, key string) (values []int) {
+	if err := r.ParseForm(); err != nil{
+		return nil
+	}
+	for _, s := range r.Form[key]{
+		val, err := strconv.Atoi(s)
+		if err != nil {
+			continue
+		}
+		values = append(values, val)
+	}
+	return values
+}
+
+func GetArrayInt64(r *http.Request, key string) (values []int64) {
+	if err := r.ParseForm(); err != nil{
+		return nil
+	}
+	for _, s := range r.Form[key]{
+		val, err := strconv.ParseInt(s,10,64)
+		if err != nil {
+			continue
+		}
+		values = append(values, val)
+	}
+	return values
+}
+
+func GetArrayBool(r *http.Request, key string) (values []int64) {
+	if err := r.ParseForm(); err != nil{
+		return nil
+	}
+	for _, s := range r.Form[key]{
+		val, err := strconv.ParseInt(s,10,64)
+		if err != nil {
+			continue
+		}
+		values = append(values, val)
+	}
+	return values
+}
+
+func GetArrayFloat32(r *http.Request, key string) (values []float32) {
+	if err := r.ParseForm(); err != nil{
+		return nil
+	}
+	for _, s := range r.Form[key]{
+		val, err := strconv.ParseFloat(s,32)
+		if err != nil {
+			continue
+		}
+		values = append(values, float32(val))
+	}
+	return values
+}
+
+func GetArrayFloat64(r *http.Request, key string) (values []float64) {
+	if err := r.ParseForm(); err != nil{
+		return nil
+	}
+	for _, s := range r.Form[key]{
+		val, err := strconv.ParseFloat(s, 64)
+		if err != nil {
+			continue
+		}
+		values = append(values, val)
+	}
+	return values
+}
